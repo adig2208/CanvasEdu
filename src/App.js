5f308520b01688df7bfbf279afbb0ce1d773ca27
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-21 17:03:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import Kanbas from "./Kanbas";
-import { HashRouter } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router";
-
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
 function App() {
-    return ( <
-        HashRouter >
-        <
-        div >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Kanbas / > }
-        /> <
-        /Routes> <
-        /div> <
-        /HashRouter>
-    );
+  return (
+    <HashRouter>
+  <div>
+    <Routes>
+      <Route path="/"         element={<Navigate to="/kanbas"/>}/>
+      <Route path="/kanbas/*" element={<Kanbas/>}/>
+    </Routes>
+  </div>
+</HashRouter>
+  );
 }
 export default App;
